fe50df3ccbe602c85656c98ac230dc82b437787a
Olympus Mons <dodgeynohangelomadula2002@gmail.com>
2021-08-05 16:24:06 +0800

    jump fail

--- main.py
+++ main.py
@@ -5,23 +5,20 @@ pygame.init()
 screen_width = 700
 screen_height = 500
 WIN = pygame.display.set_mode([screen_width, screen_height])
-pygame.display.set_caption ("aha man ka gatan-aw ug maayu. title ra gani ako gi change. HAHAHAAHAHHA")
+pygame.display.set_caption("Pygame")
 
 #! x, y,isJUMP, Player_y_ Momentum, and jumpCount is for the jump
-x = 50
-y = 50
 BG_COLOR = (128, 128, 128)
 FPS = 60
 VEL = 5
-PLAYER_y_MOMENTUM = 0
-isJump = False
-jumpCount = 2
+
 SPRITE_WIDTH, SPRITE_HIEGHT = (55, 55)
 
 
 Player_image = pygame.image.load(os.path.join('Assets', 'Player.png'))
 Player = pygame.transform.scale(Player_image, (SPRITE_WIDTH, SPRITE_HIEGHT))
- 
+
+
 def draw_window(Player_rect):
     WIN.fill(BG_COLOR)
     WIN.blit(Player, (Player_rect.x, Player_rect.y))
@@ -29,15 +26,49 @@ def draw_window(Player_rect):
 
  #TODO Add a JUMP FEATURE
 
-def player_movement(keys_pressed, Player_rect):
-    if keys_pressed[pygame.K_a] and Player_rect.x - VEL > 0: #LEFT
-        Player_rect.x -= VEL
-    if keys_pressed[pygame.K_d] and Player_rect.x - VEL < screen_width: #LEFT
-        Player_rect.x += VEL
 
+def player_movement(keys_pressed, Player_rect):
+    isJump = False
+    velocity = 5 #velocity and mass for jump: https://www.geeksforgeeks.org/python-making-an-object-jump-in-pygame/
+    mass = 1
+
+    if keys_pressed[pygame.K_a] and Player_rect.x - VEL > 0:  # LEFT
+        Player_rect.x -= VEL
+    if keys_pressed[pygame.K_d] and Player_rect.x - VEL < screen_width:  # LEFT
+        Player_rect.x += VEL
+    if isJump==False:
+        if keys_pressed[pygame.K_SPACE]:
+            isJump = True
+    if isJump:
+        # calculate force (F). F = 1 / 2 * mass * velocity ^ 2.
+        F =(1 / 2)*mass*(velocity**2)
+           
+            # change in the y co-ordinate
+        Player_rect.y-= F
+            
+            # decreasing velocity while going up and become negative while coming down
+        velocity -= 1
+            
+            # object reached its maximum height
+        if velocity<0:
+                
+                # negative sign is added to counter negative velocity
+            mass -= 1
+    
+            # objected reaches its original state
+        if velocity ==-6:
+    
+                # making isjump equal to false 
+            isJump = False
+    
+        
+                # setting original values to v and m
+            velocity = 5
+            mass = 1
   #TODO FIX CLOCK for jump
 
-def main ():
+
+def main():
     Player_rect = pygame.Rect(100, 300, SPRITE_WIDTH, SPRITE_HIEGHT)
 
     clock = pygame.time.Clock()
@@ -47,7 +78,7 @@ def main ():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            
+
         keys_pressed = pygame.key.get_pressed()
         player_movement(keys_pressed, Player_rect)
         draw_window(Player_rect)
@@ -57,5 +88,3 @@ def main ():
 
 if __name__ == "__main__":
   main()
-
-  
